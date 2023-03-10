bf476e6257f86d23458778405aae40da718bb838
Hoxton Hsu <hoxton.hsu@eunodata.com>
2023-03-10 18:49:58 +0800

    add: sql module done

--- src/main/java/com/hoxton/databaseconnectionweb/dao/MySQLDaoImpl.java
+++ src/main/java/com/hoxton/databaseconnectionweb/dao/MySQLDaoImpl.java
@@ -1,11 +1,15 @@
 package com.hoxton.databaseconnectionweb.dao;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hoxton.databaseconnectionweb.model.vo.DatabaseStatusVO;
 import org.springframework.stereotype.Repository;
 
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Hoxton on 2023/3/8
@@ -13,14 +17,16 @@ import java.util.List;
  */
 @Repository
 public class MySQLDaoImpl implements DatabaseEngineDao {
-    String url = "jdbc:mysql://localhost:3306";
+    String url = "jdbc:mysql://localhost:3306/school";
     String username = "root";
-    private String databaseEngineName;
-
+    String password = "root";
+    private String databaseEngineName="MySql";
+    Connection connection;
 
     @Override
     public DatabaseEngineDao connect() throws SQLException {
-        return null;
+        connection = DriverManager.getConnection(url, username, password);
+        return this;
     }
 
     @Override
@@ -30,7 +36,25 @@ public class MySQLDaoImpl implements DatabaseEngineDao {
 
     @Override
     public String query(String query) throws SQLException, JsonProcessingException {
-        return null;
+        List<Map<String, Object>> result = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+            while (resultSet.next()) {
+                HashMap<String, Object> row = new HashMap<>();
+                ResultSetMetaData metaData = resultSet.getMetaData();
+                int columnCount = metaData.getColumnCount();
+                for (int i = 1; i <= columnCount; i++) {
+                    String columnName = metaData.getColumnName(i);
+                    Object value = resultSet.getObject(columnName);
+                    row.put(columnName, value);
+                }
+                result.add(row);
+            }
+        }
+
+        return mapper.writeValueAsString(result);
     }
 
     @Override
